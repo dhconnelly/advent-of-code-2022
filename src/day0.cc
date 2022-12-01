5322ab91210b4a332977081baf82d1ae6f081767
Daniel Connelly <dhconnelly@gmail.com>
2022-11-30 21:33:12 -0500

    add line parsing util

--- src/day0.cc
+++ src/day0.cc
@@ -1,9 +1,14 @@
+#include <fstream>
 #include <iostream>
+#include <numeric>
 
 #include "util.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day0 <input>");
-    std::cout << kFoo << std::endl;
+    std::ifstream ifs(argv[1]);
+    auto nums = parse_lines(ifs, [](const auto& s) { return std::stol(s); });
+    auto sum = std::accumulate(nums.begin(), nums.end(), 0);
+    std::cout << sum << std::endl;
     return 0;
 }
--- src/util.h
+++ src/util.h
@@ -1,14 +1,25 @@
 #ifndef SRC_UTIL_H_
 #define SRC_UTIL_H_
 
+#include <cinttypes>
 #include <cstdlib>
+#include <istream>
+#include <ranges>
 #include <string_view>
+#include <vector>
 
 void die(const std::string_view msg) {
     std::cerr << msg << std::endl;
     std::exit(1);
 }
 
-constexpr int kFoo = 7;
+template <typename F>
+auto parse_lines(std::istream& is, F parse_line) {
+    std::string line;
+    std::vector<decltype(parse_line(line))> lines;
+    while (std::getline(is, line)) lines.push_back(parse_line(line));
+    if (is.bad()) die(strerror(errno));
+    return lines;
+}
 
 #endif  // SRC_UTIL_H_
