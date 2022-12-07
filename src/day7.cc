1215cb0f184e92a5706376d5c416fe0eec117ed6
Daniel Connelly <dhconnelly@gmail.com>
2022-12-07 08:44:03 -0500

    day7: part2

--- src/day7.cc
+++ src/day7.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <map>
 #include <memory>
 #include <numeric>
@@ -12,6 +13,7 @@
 struct dir_entry {
     std::map<std::string, int64_t> files;
     std::map<std::string, std::unique_ptr<dir_entry>> dirs;
+    int64_t total_size;
 };
 
 std::unique_ptr<dir_entry> parse_dir(std::istream& is) {
@@ -49,7 +51,7 @@ std::unique_ptr<dir_entry> parse_filesystem(std::istream& is) {
     return parse_dir(is);
 }
 
-std::pair<int64_t, int64_t> sum_total_sizes(const dir_entry& dir, int64_t max) {
+std::pair<int64_t, int64_t> sum_total_sizes(dir_entry& dir, int64_t max) {
     int64_t files_size = std::accumulate(
         dir.files.begin(), dir.files.end(), 0,
         [](int64_t sum, const auto& file) { return sum + file.second; });
@@ -61,13 +63,28 @@ std::pair<int64_t, int64_t> sum_total_sizes(const dir_entry& dir, int64_t max) {
             return sum + subdir;
         });
     int64_t total_size = files_size + dirs_size;
+    dir.total_size = total_size;
     if (total_size <= max) sizes_smaller += total_size;
     return {total_size, sizes_smaller};
+}
+
+int64_t find_deletion(const dir_entry& dir, int64_t free, int64_t needed) {
+    int64_t deletion = std::numeric_limits<int64_t>::max();
+    if ((free + dir.total_size) >= needed) {
+        deletion = std::min(deletion, dir.total_size);
+    }
+    for (const auto& [_, subdir] : dir.dirs) {
+        auto subdir_deletion = find_deletion(*subdir, free, needed);
+        deletion = std::min(deletion, subdir_deletion);
+    }
+    return deletion;
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day7 <file>");
     std::ifstream ifs(argv[1]);
     auto root = parse_filesystem(ifs);
-    std::cout << sum_total_sizes(*root, 100000).second << std::endl;
+    auto [total, smaller] = sum_total_sizes(*root, 100000);
+    std::cout << smaller << std::endl;
+    std::cout << find_deletion(*root, 70000000 - total, 30000000) << std::endl;
 }
