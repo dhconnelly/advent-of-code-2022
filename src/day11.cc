6718f8bc4b128791fc43c3c75f3585849b5ccfb2
Daniel Connelly <dhconnelly@gmail.com>
2022-12-11 10:49:25 -0500

    day11: simplify further

--- src/day11.cc
+++ src/day11.cc
@@ -14,52 +14,43 @@ enum class arg_type { lit, old };
 enum class binary_op { add, mul };
 struct operation {
     binary_op op;
-    arg_type arg_types[2];
-    int64_t lit_vals[2];
+    arg_type args[2];
+    int64_t lits[2];
 };
 int64_t evaluate(const operation& op, int64_t old) {
-    int64_t left = op.arg_types[0] == arg_type::lit ? op.lit_vals[0] : old;
-    int64_t right = op.arg_types[1] == arg_type::lit ? op.lit_vals[1] : old;
+    int64_t left = op.args[0] == arg_type::lit ? op.lits[0] : old;
+    int64_t right = op.args[1] == arg_type::lit ? op.lits[1] : old;
     return op.op == binary_op::add ? left + right : left * right;
 }
 
-struct item {
-    int64_t worry_level;
-};
-
 struct monkey {
-    int id;
-    std::list<item> items;
+    std::list<int64_t> items;
     operation worry_operation;
     int64_t test_modulus;
     int true_dest, false_dest;
     int inspections = 0;
 };
 
-void turn(std::vector<monkey>& monkeys, monkey& m, int modulus,
-          bool reduce_stress) {
+void turn(std::vector<monkey>& monkeys, monkey& m, int modulus, bool reduce) {
     for (auto it = m.items.begin(); it != m.items.end();) {
         m.inspections++;
-        item i = *it;
-        i.worry_level = evaluate(m.worry_operation, i.worry_level);
-        if (reduce_stress) i.worry_level /= 3;
-        else i.worry_level %= modulus;
+        *it = evaluate(m.worry_operation, *it);
+        *it = reduce ? *it / 3 : *it % modulus;
+        int dest = *it % m.test_modulus ? m.false_dest : m.true_dest;
+        monkeys[dest].items.push_back(*it);
         it = m.items.erase(it);
-        int dest = i.worry_level % m.test_modulus ? m.false_dest : m.true_dest;
-        monkeys[dest].items.push_back(i);
     }
 }
 
-void round(std::vector<monkey>& monkeys, int modulus, bool reduce_stress) {
-    for (auto& monkey : monkeys) turn(monkeys, monkey, modulus, reduce_stress);
+void round(std::vector<monkey>& monkeys, int modulus, bool reduce) {
+    for (auto& monkey : monkeys) turn(monkeys, monkey, modulus, reduce);
 }
 
-int64_t monkey_business(const std::vector<monkey>& monkeys) {
-    std::vector monkeys2(monkeys);
-    std::sort(monkeys2.begin(), monkeys2.end(), [](auto& m1, auto& m2) {
+int64_t monkey_business(std::vector<monkey>& monkeys) {
+    std::sort(monkeys.begin(), monkeys.end(), [](auto& m1, auto& m2) {
         return m1.inspections > m2.inspections;
     });
-    return monkeys2[0].inspections * monkeys2[1].inspections;
+    return monkeys[0].inspections * monkeys[1].inspections;
 }
 
 int modulus(const std::vector<monkey>& monkeys) {
@@ -68,12 +59,12 @@ int modulus(const std::vector<monkey>& monkeys) {
         [](int mod, auto& m) { return mod * m.test_modulus; });
 }
 
-std::list<item> parse_items(const std::string& s) {
-    std::list<item> items;
+std::list<int64_t> parse_items(const std::string& s) {
+    std::list<int64_t> items;
     static const std::regex item_pat(R"((, )?(\d+))");
     std::sregex_iterator begin(s.begin(), s.end(), item_pat), end;
     for (auto it = begin; it != end; ++it) {
-        items.push_back(item{std::stoll((*it)[2].str())});
+        items.push_back(std::stoll((*it)[2].str()));
     }
     return items;
 }
@@ -81,61 +72,54 @@ std::list<item> parse_items(const std::string& s) {
 operation parse_operation(const std::string& s) {
     static const std::regex expr_pat(R"(((\d+)|old) (\+|\*) ((\d+)|old))");
     std::smatch m;
-    if (!std::regex_match(s, m, expr_pat)) die("bad operation: " + s);
+    if (!std::regex_search(s, m, expr_pat)) die("bad operation: " + s);
     operation op;
-    op.arg_types[0] = *m[1].first == 'o' ? arg_type::old : arg_type::lit;
-    if (op.arg_types[0] == arg_type::lit) {
-        op.lit_vals[0] = std::stoll(m[1].str());
-    }
+    op.args[0] = *m[1].first == 'o' ? arg_type::old : arg_type::lit;
+    if (op.args[0] == arg_type::lit) op.lits[0] = std::stoll(m[1].str());
     op.op = *m[3].first == '*' ? binary_op::mul : binary_op::add;
-    op.arg_types[1] = *m[4].first == 'o' ? arg_type::old : arg_type::lit;
-    if (op.arg_types[1] == arg_type::lit) {
-        op.lit_vals[1] = std::stoll(m[4].str());
-    }
+    op.args[1] = *m[4].first == 'o' ? arg_type::old : arg_type::lit;
+    if (op.args[1] == arg_type::lit) op.lits[1] = std::stoll(m[4].str());
     return op;
 }
 
-void eatline(std::istream& is, std::string& buf) {
+int parse_num(const std::string& s) {
+    static const std::regex pat(R"((\d+))");
+    std::smatch m;
+    if (!std::regex_search(s, m, pat)) die("bad num");
+    return std::stoi(m[1].str());
+}
+
+std::string& eatline(std::istream& is, std::string& buf) {
     if (!std::getline(is, buf)) die(std::string("getline: ") + strerror(errno));
+    return buf;
 }
 
 std::vector<monkey> parse(std::istream&& is) {
     std::vector<monkey> monkeys;
     std::string line;
-    for (int i = 0; !is.eof(); i++) {
-        // header
+    while (!is.eof()) {
         eatline(is, line);
-        if (is.eof()) break;
-        // items
-        eatline(is, line);
-        auto items = parse_items(line.substr(line.find(':') + 2));
-        // operation
-        eatline(is, line);
-        auto op = parse_operation(line.substr(line.find('=') + 2));
-        // test
-        eatline(is, line);
-        int64_t test_modulus = std::stoll(line.substr(line.find("by") + 3));
-        // destinations
-        eatline(is, line);
-        int true_dest = std::stoi(line.substr(line.find("monkey") + 7));
-        eatline(is, line);
-        int false_dest = std::stoi(line.substr(line.find("monkey") + 7));
-        // delimiter can be empty
-        std::getline(is, line);
-        monkeys.push_back({i, items, op, test_modulus, true_dest, false_dest});
+        auto items = parse_items(eatline(is, line));
+        auto op = parse_operation(eatline(is, line));
+        int64_t test_modulus = parse_num(eatline(is, line));
+        int true_dest = parse_num(eatline(is, line));
+        int false_dest = parse_num(eatline(is, line));
+        std::getline(is, line);  // delimiter can be empty
+        monkeys.push_back({items, op, test_modulus, true_dest, false_dest});
     }
     return monkeys;
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day11 <file>");
-    auto monkeys1 = parse(std::ifstream(argv[1]));
-    auto monkeys2 = monkeys1;
-    int mod = modulus(monkeys1);
+    auto monkeys = parse(std::ifstream(argv[1]));
+    int mod = modulus(monkeys);
 
+    auto monkeys1 = monkeys;
     for (int i = 0; i < 20; i++) round(monkeys1, mod, true);
     std::cout << monkey_business(monkeys1) << std::endl;
 
+    auto monkeys2 = monkeys;
     for (int i = 0; i < 10000; i++) round(monkeys2, mod, false);
     std::cout << monkey_business(monkeys2) << std::endl;
 }
