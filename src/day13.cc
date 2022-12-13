a07d7a039a48bd38641b5d18efceea86b1887e15
Daniel Connelly <dhconnelly@gmail.com>
2022-12-13 10:42:17 -0500

    day13: part1

--- src/day13.cc
+++ src/day13.cc
@@ -12,18 +12,16 @@ struct item {
     enum class type { num, packet };
     type typ;
     int num;
-    packet packet;
+    packet p;
+    static item from(int num) { return {.typ = type::num, .num = num}; }
+    static item from(packet p) { return {.typ = type::packet, .p = p}; }
 };
 
 packet parse_packet(std::istream& is);
 item parse_item(std::istream& is) {
-    if (is.peek() == '[') {
-        return item{.typ = item::type::packet, .packet = parse_packet(is)};
-    } else {
-        item i{.typ = item::type::num};
-        is >> i.num;
-        return i;
-    }
+    if (is.peek() == '[') return item::from(parse_packet(is));
+    else if (int num; is >> num) return item::from(num);
+    else die("bad item");
 }
 
 packet parse_packet(std::istream& is) {
@@ -49,15 +47,62 @@ std::vector<std::pair<packet, packet>> parse(std::istream&& is) {
     return packet_pairs;
 }
 
-int compare(const packet& left, const packet& right) { return 1; }
+int compare(item& left, item& right);
+int compare(packet& left, packet& right) {
+    int i;
+    for (i = 0; i < left.size() && i < right.size(); i++) {
+        if (int d = compare(left[i], right[i]); d < 0) return -1;
+        else if (d > 0) return 1;
+    }
+    if (i == left.size()) return -1;
+    if (i == right.size()) return 1;
+    return 0;
+}
+
+void print(const item& i) {
+    switch (i.typ) {
+        case item::type::num: std::cout << i.num << ' '; break;
+        case item::type::packet:
+            std::cout << '(';
+            for (int j = 0; j < i.p.size(); j++) {
+                print(i.p[j]);
+            }
+            std::cout << ')';
+            break;
+    }
+}
+
+int compare(item& left, item& right) {
+    std::cout << "comparing: ";
+    print(left);
+    std::cout << " with ";
+    print(right);
+    std::cout << std::endl;
+    if (left.typ == item::type::num && right.typ == item::type::num) {
+        return left.num - right.num;
+    } else if (left.typ == item::type::packet &&
+               right.typ == item::type::packet) {
+        return compare(left.p, right.p);
+    } else if (left.typ == item::type::num) {
+        packet p;
+        p.push_back(left);
+        return compare(p, right.p);
+    } else {
+        packet p;
+        p.push_back(right);
+        return compare(left.p, p);
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day13 <file>");
     auto packet_pairs = parse(std::ifstream(argv[1]));
     int sum = 0;
-    for (int i = 1; i <= packet_pairs.size(); i++) {
-        if (compare(packet_pairs[i].first, packet_pairs[i].second) == 0) {
-            sum += i;
+    for (int i = 0; i < packet_pairs.size(); i++) {
+        std::cout << "pair " << i + 1 << std::endl;
+        if (compare(packet_pairs[i].first, packet_pairs[i].second) < 0) {
+            std::cout << "right order!\n";
+            sum += i + 1;
         }
     }
     std::cout << sum << std::endl;
