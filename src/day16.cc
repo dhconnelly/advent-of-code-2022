3554025b37c7df850450381d48557fe5c57ccc09
Daniel Connelly <dhconnelly@gmail.com>
2022-12-16 01:01:35 -0500

    day16: bfs

--- src/day16.cc
+++ src/day16.cc
@@ -1,7 +1,9 @@
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <regex>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -31,6 +33,30 @@ std::pair<flow_rates, connections> parse(std::istream&& is) {
     return {rates, conns};
 }
 
+using edge = std::pair<std::string, int64_t>;
+std::map<std::string, std::vector<edge>> shortest_dists(
+    const flow_rates& rates, const connections& conns) {
+    std::map<std::string, std::vector<edge>> dists;
+    for (const auto& [valve, valves] : conns) {
+        auto& edges = dists[valve];
+        std::deque<edge> q;
+        q.emplace_back(valve, 0);
+        std::set<std::string> v;
+        v.insert(valve);
+        while (!q.empty()) {
+            auto [cur, d] = q.front();
+            q.pop_front();
+            for (const auto& nbr : conns.at(cur)) {
+                if (v.count(nbr)) continue;
+                v.insert(nbr);
+                edges.emplace_back(nbr, d + 1);
+                q.emplace_back(nbr, d + 1);
+            }
+        }
+    }
+    return dists;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day16 <file>");
     auto [flow_rates, connections] = parse(std::ifstream(argv[1]));
@@ -42,5 +68,11 @@ int main(int argc, char* argv[]) {
         std::cout << valve << ": ";
         for (const auto& valve : valves) std::cout << valve << ", ";
         std::cout << std::endl;
+    }
+    auto dists = shortest_dists(flow_rates, connections);
+    for (const auto& [from, valves] : dists) {
+        for (const auto& [to, d] : valves) {
+            std::cout << from << " -> " << to << ": " << d << std::endl;
+        }
     }
 }
