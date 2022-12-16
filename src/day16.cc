226c89671fed014d2ed34a3be88755d3cabdbebf
Daniel Connelly <dhconnelly@gmail.com>
2022-12-16 01:29:45 -0500

    day16: part1 memoized still not fast enough

--- src/day16.cc
+++ src/day16.cc
@@ -34,9 +34,9 @@ std::pair<flow_rates, connections> parse(std::istream&& is) {
 }
 
 using edge = std::pair<std::string, int64_t>;
-std::map<std::string, std::vector<edge>> shortest_dists(
-    const flow_rates& rates, const connections& conns) {
-    std::map<std::string, std::vector<edge>> dists;
+using edges = std::map<std::string, std::vector<edge>>;
+edges shortest_dists(const flow_rates& rates, const connections& conns) {
+    edges dists;
     for (const auto& [valve, valves] : conns) {
         auto& edges = dists[valve];
         std::deque<edge> q;
@@ -57,22 +57,75 @@ std::map<std::string, std::vector<edge>> shortest_dists(
     return dists;
 }
 
+std::string key(const std::string& cur, int minute,
+                std::set<std::string>& open) {
+    std::string k(cur);
+    k.push_back('|');
+    k.append(std::to_string(minute));
+    k.push_back('|');
+    for (const auto& valve : open) {
+        k.append(valve);
+        k.push_back(',');
+    }
+    return k;
+}
+
+int64_t max_released(const edges& edges, const flow_rates& rates,
+                     const std::string& cur, int minute, int max_minute,
+                     int64_t releasing, std::set<std::string>& open,
+                     std::map<std::string, int64_t>& memo) {
+    if (minute > max_minute) return 0;
+    std::string k = key(cur, minute, open);
+    if (auto it = memo.find(k); it != memo.end()) return it->second;
+    int64_t released = std::numeric_limits<int64_t>::min();
+    // std::cout << "minute " << minute << ", at " << cur << " releasing "
+    //<< releasing << std::endl;
+    for (const auto& [valve, dist] : edges.at(cur)) {
+        if (open.count(valve)) continue;
+        if (minute + dist + 1 > max_minute) continue;
+        // move there and open it
+        // std::cout << "opening " << valve << std::endl;
+        open.insert(valve);
+        int64_t before = releasing * (dist + 1);
+        int64_t after =
+            max_released(edges, rates, valve, minute + dist + 1, max_minute,
+                         releasing + rates.at(valve), open, memo);
+        released = std::max(released, before + after);
+        open.erase(valve);
+    }
+    // what if we do nothing
+    released = std::max(released, releasing * (max_minute - minute + 1));
+    memo[k] = released;
+    return released;
+}
+
+int64_t max_released(const edges& edges, const flow_rates& rates) {
+    std::set<std::string> open;
+    std::map<std::string, int64_t> memo;
+    return max_released(edges, rates, "AA", 1, 30, 0, open, memo);
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day16 <file>");
     auto [flow_rates, connections] = parse(std::ifstream(argv[1]));
-    for (const auto& [valve, rate] : flow_rates) {
-        std::cout << valve << ": " << rate << std::endl;
-    }
-    std::cout << "valves:\n";
-    for (const auto& [valve, valves] : connections) {
-        std::cout << valve << ": ";
-        for (const auto& valve : valves) std::cout << valve << ", ";
-        std::cout << std::endl;
-    }
     auto dists = shortest_dists(flow_rates, connections);
-    for (const auto& [from, valves] : dists) {
-        for (const auto& [to, d] : valves) {
-            std::cout << from << " -> " << to << ": " << d << std::endl;
+
+    /*
+        for (const auto& [valve, rate] : flow_rates) {
+            std::cout << valve << ": " << rate << std::endl;
         }
-    }
+        std::cout << "valves:\n";
+        for (const auto& [valve, valves] : connections) {
+            std::cout << valve << ": ";
+            for (const auto& valve : valves) std::cout << valve << ", ";
+            std::cout << std::endl;
+        }
+        for (const auto& [from, valves] : dists) {
+            for (const auto& [to, d] : valves) {
+                std::cout << from << " -> " << to << ": " << d << std::endl;
+            }
+        }
+    */
+
+    std::cout << max_released(dists, flow_rates) << std::endl;
 }
