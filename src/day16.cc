ccd924060601b229e9dd2c10297e7a65bc41d5a7
Daniel Connelly <dhconnelly@gmail.com>
2022-12-16 09:00:45 -0500

    day16: part1

--- src/day16.cc
+++ src/day16.cc
@@ -13,30 +13,9 @@
 
 using connections_map = std::map<std::string, std::vector<std::string>>;
 using flow_rates_map = std::map<std::string, int64_t>;
-
-std::pair<flow_rates_map, connections_map> parse(std::istream&& is) {
-    flow_rates_map rates;
-    connections_map conns;
-    for (std::string line; std::getline(is, line);) {
-        static const std::regex pat(
-            R"(Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$)");
-        std::smatch m;
-        if (!std::regex_match(line, m, pat)) die("bad line: " + line);
-        auto valve = m[1].str();
-        rates[valve] = std::stoi(m[2].str());
-        auto valves = m[3].str();
-        static const std::regex valve_pat(R"((, )?(\w+))");
-        for (std::sregex_iterator it(valves.begin(), valves.end(), valve_pat),
-             end;
-             it != end; it++) {
-            conns[valve].push_back((*it)[2].str());
-        }
-    }
-    return {rates, conns};
-}
-
 using edge = std::pair<std::string, int64_t>;
 using edges = std::map<std::string, std::vector<edge>>;
+
 edges shortest_dists(const flow_rates_map& rates,
                      const connections_map& conns) {
     edges dists;
@@ -67,25 +46,16 @@ std::tuple<flow_rates, dists, int> prune(const flow_rates_map& rates_map,
     flow_rates rates;
     std::map<std::string, int> idx;
     for (const auto& [valve, rate] : rates_map) {
-        if (rate == 0) continue;
+        if (rate == 0 && valve != "AA") continue;
         idx[valve] = rates.size();
         rates.push_back(rate);
     }
-    std::cout << "reduced size from " << rates_map.size() << " to "
-              << rates.size() << std::endl;
     dists dists(rates.size(), std::vector<int64_t>(rates.size(), -1));
     for (const auto& [from, es] : edges) {
-        if (rates_map.at(from) == 0) continue;
+        if (rates_map.at(from) == 0 && from != "AA") continue;
         dists[idx[from]][idx[from]] = 0;
         for (const auto& [to, dist] : es) {
             if (rates_map.at(to) == 0) continue;
-            std::cout << "===" << std::endl;
-            std::cout << "dist(" << from << ", " << to << ") = " << dist
-                      << std::endl;
-            std::cout << "dist(" << idx[from] << ", " << idx[to]
-                      << ") = " << dist << std::endl;
-            std::cout << dists.size() << "," << dists[idx[from]].size()
-                      << std::endl;
             dists[idx[from]][idx[to]] = dist;
         }
     }
@@ -136,28 +106,31 @@ int64_t max_released(const dists& dists, const flow_rates& rates, int start) {
     return max_released(dists, rates, start, 1, 30, 0, open, memo);
 }
 
+std::pair<flow_rates_map, connections_map> parse(std::istream&& is) {
+    flow_rates_map rates;
+    connections_map conns;
+    for (std::string line; std::getline(is, line);) {
+        static const std::regex pat(
+            R"(Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$)");
+        std::smatch m;
+        if (!std::regex_match(line, m, pat)) die("bad line: " + line);
+        auto valve = m[1].str();
+        rates[valve] = std::stoi(m[2].str());
+        auto valves = m[3].str();
+        static const std::regex valve_pat(R"((, )?(\w+))");
+        for (std::sregex_iterator it(valves.begin(), valves.end(), valve_pat),
+             end;
+             it != end; it++) {
+            conns[valve].push_back((*it)[2].str());
+        }
+    }
+    return {rates, conns};
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day16 <file>");
     auto [flow_rates_map, connections_map] = parse(std::ifstream(argv[1]));
     auto edges = shortest_dists(flow_rates_map, connections_map);
     auto [flow_rates, dists, start] = prune(flow_rates_map, edges);
-
-    /*
-        for (const auto& [valve, rate] : flow_rates) {
-            std::cout << valve << ": " << rate << std::endl;
-        }
-        std::cout << "valves:\n";
-        for (const auto& [valve, valves] : connections) {
-            std::cout << valve << ": ";
-            for (const auto& valve : valves) std::cout << valve << ", ";
-            std::cout << std::endl;
-        }
-        for (const auto& [from, valves] : dists) {
-            for (const auto& [to, d] : valves) {
-                std::cout << from << " -> " << to << ": " << d << std::endl;
-            }
-        }
-    */
-
     std::cout << max_released(dists, flow_rates, start) << std::endl;
 }
