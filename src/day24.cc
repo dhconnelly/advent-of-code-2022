9bb165a225ed92a3e029b694fd91fbde28de7eeb
Daniel Connelly <dhconnelly@gmail.com>
2022-12-24 08:50:30 -0500

    day24: find entrance/exit

--- src/day24.cc
+++ src/day24.cc
@@ -1,11 +1,17 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "util.h"
 
 using tile = char;
 using tile_members = std::vector<tile>;
 using grid = std::vector<std::vector<tile_members>>;
+using pt = std::pair<int, int>;
 
 grid parse(std::istream&& is) {
     grid g;
@@ -32,8 +38,27 @@ void print(const grid& g) {
     }
 }
 
+void print(pt p) { std::cout << '[' << p.first << ',' << p.second << ']'; }
+
+std::pair<pt, pt> find_entrance_exit(const grid& g) {
+    auto find_empty = [](const auto& c) {
+        auto it = std::find_if(c.begin(), c.end(), [](const auto& at_tile) {
+            return at_tile.empty();
+        });
+        if (it == c.end()) die("no empty tile found");
+        return it;
+    };
+    size_t entrance_col = find_empty(g.front()) - g.front().begin();
+    size_t exit_col = find_empty(g.back()) - g.back().begin();
+    return {{0, entrance_col}, {g.size() - 1, exit_col}};
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) die("usage: day24 <file>");
     auto g = parse(std::ifstream(argv[1]));
     print(g);
+    auto [src, dst] = find_entrance_exit(g);
+    print(src);
+    print(dst);
+    std::cout << std::endl;
 }
